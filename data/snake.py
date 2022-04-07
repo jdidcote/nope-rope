126bb3326fc8625b61ff9367ce4c6d3b49252aa7
jdidcote <joshua.didcote@outlook.com>
2022-04-07 21:44:15 +0100

    Updated snake class to have change_direction method and added a variable current_gamespeed

--- data/snake.py
+++ data/snake.py
@@ -6,15 +6,30 @@ from data.position import Position
 class Snake:
     def __init__(self, pos: List[Position]):
         self.pos = pos
+        self.current_direction = None
 
-    def move(self):
+    def change_direction(self, direction: str):
+        """Change the direction of the snake's movement
+        """
+
+        # Change the snakes current direction
+        self.current_direction = direction
+
+    def move(self, direction: str, velocity):
         """Add a new element to head of list in given direction
         """
-        pass
+
+        # If no direction currently set, don't move
+        if self.current_direction is None:
+            return
+
+        if self.current_direction == "UP":
+            self.pos[-1].y -= velocity
 
     def grow(self):
         """Add a new node to the start of the list and do nothing else
         """
+        pass
 
     def reset(self):
         pass
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 import pygame
 
 from data.snake import Position, Snake
@@ -33,26 +35,46 @@ def draw_snake(snake: Snake):
         segment_rect = pygame.Rect(segment.x, segment.y, GRID_SIZE, GRID_SIZE)
         pygame.draw.rect(WIN, SNAKE_COLOR, segment_rect)
 
+# def move_snake(keys_pressed, snake: Snake):
+#     if keys_pressed[pygame.K_a] and yellow.x - VEL > 0:  # LEFT
+#         yellow.x -= VEL
+#     if keys_pressed[pygame.K_d] and yellow.x + VEL + yellow.width < BORDER.x:  # RIGHT
+#         yellow.x += VEL
+#     if keys_pressed[pygame.K_w] and yellow.y - VEL > 0:  # UP
+#         yellow.y -= VEL
+#     if keys_pressed[pygame.K_s] and yellow.y + VEL + yellow.height < HEIGHT - 15:  # DOWN
+#         yellow.y += VEL
+
 
 def main():
     clock = pygame.time.Clock()
-    WIN.fill(BG_COLOUR)
-
-    # Initialise snake object
 
     grid = Grid(GRID_SIZE)
     snake = Snake([INITIAL_SNAKE_POS])
 
     run = True
     while run:
+        WIN.fill(BG_COLOUR)
         grid.draw()
         clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
 
+        keys_pressed = pygame.key.get_pressed()
+
+        if keys_pressed[pygame.K_UP]:  # LEFT
+            snake.change_direction("UP")
+
+        current_gamespeed = 0.5  # THIS WILL SCALE DOWN WITH CURRENT SNAKE LENGTH
+        if snake.current_direction is not None:
+            sleep(current_gamespeed)
+
+        snake.move("UP", GRID_SIZE)
+
         draw_snake(snake)
         pygame.display.update()
+
 
     pygame.quit()
 
